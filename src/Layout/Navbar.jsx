5c13b85a74b31c722ec2b19e03adc9b29b538733
waqasansarii <65023462+waqasansarii@users.noreply.github.com>
2021-06-09 18:37:12 +0500

    updated

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,45 +1,67 @@
 import React from "react";
+import logo from '../Assets/final.svg'
 import "./style.css";
 
 const Navbar = () => {
-    window.addEventListener("scroll", function () {
-        var header = document.querySelector(".nav_container");
-        header.classList.toggle("sticky", window.scrollY > 100);
-      });
+  window.addEventListener("scroll", function () {
+    var header = document.querySelector(".nav_container");
+    header.classList.toggle("sticky", window.scrollY > 100);
+  });
   return (
-    <div className='nav_container'>
-      <nav className="navbar navbar-expand-lg navbar-light c_main_nav ">
+    <div className="nav_container">
+      <nav className="navbar navbar-expand-lg navbar-light ">
         <div className="container-fluid">
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse custom_nav" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse " id="navbarTogglerDemo01">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
-                  Home
+                  Product
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Link
+                  Resources
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Inspiration
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
                 </a>
               </li>
             </ul>
+            <div>
+
             <a className="navbar-brand" href="#">
-              Navbar
+              <img className='logo' src={logo} alt="..." />
             </a>
-            <div className="buttons_div">
-                <button>Login</button>
-                <button>Sign up  free</button>
+            </div>
+            <div className="d-flex">
+              <div>
+                <a className="Login_btn" href="#">
+                  Login
+                </a>
+              </div>
+              <div>
+                <a className="Login_btn bg_clr_btn" href="#">
+                  Sign up Free
+                </a>
+              </div>
             </div>
           </div>
         </div>
